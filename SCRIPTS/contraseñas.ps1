47dc266fb1fd1fd8583733538776144725e2be50
Isaac <91330447+alurue9140@users.noreply.github.com>
2022-12-13 18:38:35 +0100

    Update contraseñas.ps1

--- SCRIPTS/contraseñas.ps1
+++ SCRIPTS/contraseñas.ps1
@@ -4,16 +4,16 @@
 #Se configura para que se bloquee si se falla al intento 3.
 net accounts /lockoutthreshold:3
 #Duración del bloqueo 60 minutos
-net accounts /lockoutduration:60
+net accounts /lockoutduration:120
 #Ventana de bloqueo (tiempo en minutos antes de que se restablezca el contador de intentos fallidos de contraseña )
-net accounts /lockoutwindow:10
+net accounts /lockoutwindow:60
 
 #Establecer los dias máximos para cambiar el password.
-net accounts /maxpwage:30
+net accounts /maxpwage:14
 #Establecer los dias mínimos para cambiar el password.
-net accounts /minpwage:1
+net accounts /minpwage:5
 #Establecer la longitud mínima del password.
-net accounts /minpwlen:10
+net accounts /minpwlen:8
 #Establecer la cantidad de contraseñas a guardar
 net accounts /uniquepw:5
 #Exportar la politica de contraseñas para poder activar la complejidad de caracteres (ya que esta no se puede configurar mediante un comando comun)
